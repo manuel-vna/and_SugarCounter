f930be378a6b965fbced7c13a3476a298c822651
von Au, Manuel <manuel.von.au@gdata.de>
2024-05-11 23:51:43 +0200

    #3, first 7 day graph version in XYChartUI.kt

---
+++ app/src/main/java/com/example/mva_sugarcounter/composables/XYChartUI.kt
@@ -0,0 +1,101 @@
+package com.example.mva_sugarcounter.composables
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.mva_sugarcounter.util.HelperMethods
+
+@Composable
+fun SimpleLine(exampleDate: List<Triple<Int, Int, String>>) {
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.LightGray)
+    ) {
+        Canvas(
+            modifier = Modifier
+                .fillMaxSize()
+                .aspectRatio(3 / 3f)
+                .padding(16.dp)
+        ) {
+            val barWidthPix = 1.dp.toPx()
+            drawRect(Color.Black, style = Stroke(barWidthPix))
+
+            val fortyFiveGramLine = (size.height / 100) * 55
+            drawLine(
+                Color.Red,
+                start = Offset(0f, fortyFiveGramLine),
+                end = Offset(size.width, fortyFiveGramLine),
+                strokeWidth = barWidthPix
+            )
+
+            val verticalLines = 5
+            val verticalSize = size.width / (verticalLines + 1)
+            val path = Path()
+
+            repeat(verticalLines) { i ->
+                val startX = verticalSize * (i + 1)
+                drawLine(
+                    Color.Black,
+                    start = Offset(startX, 0f),
+                    end = Offset(startX, size.height),
+                    strokeWidth = barWidthPix
+                )
+            }
+
+            exampleData.forEach {
+                var height = getXyChartFloat(it.second, size.height)
+                val startX = verticalSize * (it.first + 1)
+                path.lineTo(startX, height)
+                drawPath(path = path, color = Color.Blue, style = Stroke(2.dp.toPx()))
+
+                val dayCount = it.first
+                println("it.first: $dayCount")
+                println("startX: $startX")
+                println("height: $height")
+
+            }
+        }
+
+    }
+}
+
+
+fun getXyChartFloat(gramValue: Int, chartHeight: Float): Float {
+    return if (gramValue <= 100) {
+        (chartHeight / 100) * (100 - gramValue)
+    } else {
+        chartHeight
+    }
+}
+
+val exampleData = listOf(
+    Triple(0, 10, "2024-05-1"),
+    Triple(1, 5, "2024-05-2"),
+    Triple(2, 45, "2024-05-3"),
+    Triple(3, 45, "2024-05-4"),
+    Triple(4, 20, "2024-05-5"),
+    Triple(5, 30, "2024-05-6"),
+    Triple(6, 35, "2024-05-7")
+)
+
+@Preview
+@Composable
+fun GraphPreview() {
+    MaterialTheme {
+        SimpleLine(exampleData)
+    }
+}
