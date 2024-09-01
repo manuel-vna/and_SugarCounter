4b941267d7e70f5285a0007330cf63595ed64a38
von Au, Manuel <manuel.von.au@gdata.de>
2024-09-01 14:09:04 +0200

    #21, added FAQ content for German

--- app/src/main/java/com/jumparoundcreations/mva_sugarcounter/data/FaqDataList.kt
+++ app/src/main/java/com/jumparoundcreations/mva_sugarcounter/data/FaqDataList.kt
@@ -14,6 +14,46 @@ object faqDataList {
             id = 1,
             question = R.string.faq_question_two,
             answer = R.string.faq_answer_two
+        ),
+        Faq(
+            id = 2,
+            question = R.string.faq_question_three,
+            answer = R.string.faq_answer_three
+        ),
+        Faq(
+            id = 3,
+            question = R.string.faq_question_four,
+            answer = R.string.faq_answer_four
+        ),
+        Faq(
+            id = 4,
+            question = R.string.faq_question_five,
+            answer = R.string.faq_answer_five
+        ),
+        Faq(
+            id = 5,
+            question = R.string.faq_question_six,
+            answer = R.string.faq_answer_six
+        ),
+        Faq(
+            id = 6,
+            question = R.string.faq_question_seven,
+            answer = R.string.faq_answer_seven
+        ),
+        Faq(
+            id = 7,
+            question = R.string.faq_question_eight,
+            answer = R.string.faq_answer_eight
+        ),
+        Faq(
+            id = 8,
+            question = R.string.faq_question_nine,
+            answer = R.string.faq_answer_nine
+        ),
+        Faq(
+            id = 9,
+            question = R.string.faq_question_ten,
+            answer = R.string.faq_answer_ten
         )
 
     )
