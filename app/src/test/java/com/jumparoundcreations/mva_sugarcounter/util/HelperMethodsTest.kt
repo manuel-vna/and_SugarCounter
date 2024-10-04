5c29fb0cfe8930f554071197db6b96b1378a1d2e
von Au, Manuel <manuel.von.au@gdata.de>
2024-10-04 20:49:37 +0200

    #24, unit tests added to HelperMethodsTest.kt

--- app/src/test/java/com/jumparoundcreations/mva_sugarcounter/util/HelperMethodsTest.kt
+++ app/src/test/java/com/jumparoundcreations/mva_sugarcounter/util/HelperMethodsTest.kt
@@ -4,17 +4,22 @@ package com.jumparoundcreations.mva_sugarcounter.util
 import android.content.Context
 import android.text.format.DateUtils
 import com.jumparoundcreations.mva_sugarcounter.data.Entry
+import com.jumparoundcreations.mva_sugarcounter.data.EntryGroup
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit4.MockKRule
 import io.mockk.mockk
 import io.mockk.mockkStatic
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotEquals
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
 
 class HelperMethodsTest {
+
+    private lateinit var exampleListWithEntries: List<Entry>
 
     @get:Rule
     val mockkRule = MockKRule(this)
@@ -22,42 +27,9 @@ class HelperMethodsTest {
     @MockK
     private lateinit var mockContext: Context
 
-
-    @Test
-    fun timestampIsTodayOrYesterday_checkToday() {
-
-        mockkStatic(DateUtils::class)
-        mockk<DateUtils>()
-        every { DateUtils.isToday(any()) } returns true
-
-        val timeString =
-            HelperMethods.timestampIsTodayOrYesterday(1715801801) // 1715801801 = timestamp: Wednesday, May 15, 2024 21:36:41 GMT+02:00
-        assertEquals(timeString, HelperMethods.TodayOrYesterday.TODAY)
-    }
-
-    @Test
-    fun timestampIsTodayOrYesterday_checkLater() {
-
-
-        mockkStatic(DateUtils::class)
-        mockk<DateUtils>()
-        every { DateUtils.isToday(any()) } returns false
-
-        val timeString =
-            HelperMethods.timestampIsTodayOrYesterday(1715801801) // 1715801801 = timestamp: Wednesday, May 15, 2024 21:36:41 GMT+02:00
-        assertEquals(timeString, HelperMethods.TodayOrYesterday.LATER)
-    }
-
-    @Test
-    fun formatDateToString() {
-        val dateString = HelperMethods.formatDateToString(1716152163, "YYYY-MM-dd")
-        assertEquals(dateString, "2024-05-19")
-    }
-
-    @Test
-    fun calculateTotalGramPerDayBlock() {
-
-        val exampleListWithEntries = listOf(
+    @Before
+    fun setup() {
+        exampleListWithEntries = listOf(
             Entry(
                 id = 0,
                 currentTimestamp = 1716233571,
@@ -94,8 +66,84 @@ class HelperMethodsTest {
                 perPieceAmount = 1,
                 gramTotal = 13
             )
-        ) // gramTotal = 9 + 16 + 13 = 32
+        )
+    }
 
+    @Test
+    fun groupCounterItemsInGroupsByDayMatch() {
+        //Prepare
+        mockkStatic(DateUtils::class)
+        mockk<DateUtils>()
+        every { DateUtils.isToday(any()) } returns false
+
+        val entryGroupControl = listOf(
+            EntryGroup(
+                date = "2024-05-20",
+                dayDisplayFormat = "Monday (20.05.)",
+                entryList = exampleListWithEntries
+            )
+        )
+        //Action
+        val entryGroup = HelperMethods.groupCounterItemsInGroupsByDay(exampleListWithEntries)
+        //Validate
+        assertEquals(entryGroupControl, entryGroup)
+    }
+
+    @Test
+    fun groupCounterItemsInGroupsByDayNoMatch() {
+        //Prepare
+        mockkStatic(DateUtils::class)
+        mockk<DateUtils>()
+        every { DateUtils.isToday(any()) } returns false
+
+        val entryGroupControl = listOf(
+            EntryGroup(
+                date = "2023-05-20", // date is different
+                dayDisplayFormat = "Monday (20.05.)",
+                entryList = exampleListWithEntries
+            )
+        )
+        //Action
+        val entryGroup = HelperMethods.groupCounterItemsInGroupsByDay(exampleListWithEntries)
+        //Validate
+        assertNotEquals(entryGroupControl, entryGroup)
+    }
+
+    @Test
+    fun timestampIsTodayOrYesterday_checkToday() {
+
+        mockkStatic(DateUtils::class)
+        mockk<DateUtils>()
+        every { DateUtils.isToday(any()) } returns true
+
+        val timeString =
+            HelperMethods.timestampIsTodayOrYesterday(1715801801) // 1715801801 = timestamp: Wednesday, May 15, 2024 21:36:41 GMT+02:00
+        assertEquals(timeString, HelperMethods.TodayOrYesterday.TODAY)
+    }
+
+    @Test
+    fun timestampIsTodayOrYesterday_checkLater() {
+
+
+        mockkStatic(DateUtils::class)
+        mockk<DateUtils>()
+        every { DateUtils.isToday(any()) } returns false
+
+        val timeString =
+            HelperMethods.timestampIsTodayOrYesterday(1715801801) // 1715801801 = timestamp: Wednesday, May 15, 2024 21:36:41 GMT+02:00
+        assertEquals(timeString, HelperMethods.TodayOrYesterday.LATER)
+    }
+
+    @Test
+    fun formatDateToString() {
+        val dateString = HelperMethods.formatDateToString(1716152163, "YYYY-MM-dd")
+        assertEquals(dateString, "2024-05-19")
+    }
+
+    @Test
+    fun calculateTotalGramPerDayBlock() {
+
+        // gramTotal = 9 + 16 + 13 = 32
         val totalGramPerDayBlock =
             HelperMethods.calculateTotalGramPerDayBlock(exampleListWithEntries)
         assertEquals(totalGramPerDayBlock, 38)
