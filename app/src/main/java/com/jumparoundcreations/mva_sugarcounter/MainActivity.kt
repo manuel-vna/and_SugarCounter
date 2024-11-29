c72095943f474ca7f06356edbe0845fee9e7ff03
von Au, Manuel <manuel.von.au@gdata.de>
2024-11-29 16:24:12 +0100

    #26, forgot to remove test code from switch bug fix in SettingUI

--- app/src/main/java/com/jumparoundcreations/mva_sugarcounter/MainActivity.kt
+++ app/src/main/java/com/jumparoundcreations/mva_sugarcounter/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.jumparoundcreations.mva_sugarcounter
 
-import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,29 +10,12 @@ import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.jumparoundcreations.mva_sugarcounter.navigation.MainScreenView
 import com.jumparoundcreations.mva_sugarcounter.ui.theme.Mva_SugarCounterTheme
-import com.jumparoundcreations.mva_sugarcounter.viewModels.SettingsVM
 import org.koin.core.component.KoinComponent
-import org.koin.core.component.inject
-import org.koin.core.qualifier.named
 
 class MainActivity : ComponentActivity(), KoinComponent {
 
-    private val sharedPrefsMain by inject<SharedPreferences>(qualifier = named("sharedPrefsMain"))
-    private val settingsVM = SettingsVM()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        /*
-        // set the caloriesSwitch of SettingsUI to the correct state
-        val caloriesCounterSwitchActivated = sharedPrefsMain.getBoolean(
-            "caloriesCounterActivated",
-            false
-        )
-        Log.d("Switch","caloriesCounterSwitchActivated: $caloriesCounterSwitchActivated")
-        settingsVM.actionChangeCaloriesCounterSwitch(caloriesCounterSwitchActivated)
-
-         */
 
         enableEdgeToEdge()
 
