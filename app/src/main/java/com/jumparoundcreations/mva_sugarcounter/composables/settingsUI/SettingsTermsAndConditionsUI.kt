2b8bec8d0a61f22af2b1e7aa5d667b63aecc2b20
von Au, Manuel <manuel.von.au@gdata.de>
2025-01-02 22:24:48 +0100

    #20, making html screens scrollable + added more text content

--- app/src/main/java/com/jumparoundcreations/mva_sugarcounter/composables/settingsUI/SettingsTermsAndConditionsUI.kt
+++ app/src/main/java/com/jumparoundcreations/mva_sugarcounter/composables/settingsUI/SettingsTermsAndConditionsUI.kt
@@ -2,6 +2,8 @@ package com.jumparoundcreations.mva_sugarcounter.composables.settingsUI
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,10 +18,14 @@ fun TermsAndConditionsUI() {
 
     val termsHtml: String = getKoin().get(named("termsAndConditions"))
 
-    Column(modifier = Modifier.padding(16.dp)) {
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState())
+    ) {
         val spannedText = HtmlCompat.fromHtml(termsHtml, HtmlCompat.FROM_HTML_MODE_LEGACY)
         Text(
-            text = spannedText.toString(), // Use AnnotatedString if rich formatting is required
+            text = spannedText.toString(),
             style = MaterialTheme.typography.bodyLarge,
         )
     }
