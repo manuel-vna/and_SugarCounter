418dca9cc468d72d74aa8ecac0f25774651bc87e
von Au, Manuel <manuel.von.au@gdata.de>
2025-03-17 15:37:18 +0100

    #20,

--- app/src/main/java/com/jumparoundcreations/mva_sugarcounter/main/MainScreenView.kt
+++ app/src/main/java/com/jumparoundcreations/mva_sugarcounter/main/MainScreenView.kt
@@ -1,12 +1,9 @@
 package com.jumparoundcreations.mva_sugarcounter.main
 
-import android.annotation.SuppressLint
 import android.content.Context
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.displayCutoutPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemBars
-import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
@@ -19,24 +16,22 @@ import androidx.navigation.compose.rememberNavController
 import com.jumparoundcreations.mva_sugarcounter.navigation.BottomNavigation
 import com.jumparoundcreations.mva_sugarcounter.navigation.NavigationGraph
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+
 @Composable
 fun MainScreenView(context: Context) {
 
     val navController = rememberNavController()
     val snackbarHostState = remember { SnackbarHostState() }
     Scaffold(
+        modifier = Modifier.displayCutoutPadding(),
         bottomBar = { BottomNavigation(navController = navController) },
         snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
         floatingActionButtonPosition = FabPosition.End
     ) {
         Box(
             modifier = Modifier
-                // padding at the 'top' is completely handled by WindowInsets.systemBars
-                // padding at the 'bottom' is needed for app navigation bar
-                .padding(start = 16.dp, end = 16.dp, bottom = 90.dp)
-                // systemBars = system's top status bar and system's bottom navigation bar
-                .windowInsetsPadding(WindowInsets.systemBars)
+                .padding(it)
+                .padding(horizontal = 16.dp)
         )
         {
             NavigationGraph(
