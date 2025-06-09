65d57b3f0dc39c7d574af303a3f020e121109375
manuelvonau <vonau.manuel@gmail.com>
2025-06-09 08:55:09 +0200

    #33, debug symbols generation added to app:build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,9 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            ndk {
+                debugSymbolLevel = "FULL"
+            }
         }
     }
     compileOptions {
