2e0a879969210772f953bc37b1ca035e11ca9e96
manuelvonau <vonau.manuel@gmail.com>
2025-12-13 14:58:52 +0100

    #44, added tests

--- app/src/test/java/com/jumparoundcreations/sugarcounter/features/entrySavingFeature/EntrySavingViewModelTest.kt
+++ app/src/test/java/com/jumparoundcreations/sugarcounter/features/entrySavingFeature/EntrySavingViewModelTest.kt
@@ -2,6 +2,8 @@ package com.jumparoundcreations.sugarcounter.features.entrySavingFeature
 
 import com.jumparoundcreations.sugarcounter.data.SugarEntry
 import com.jumparoundcreations.sugarcounter.data.categoryData.Category
+import com.jumparoundcreations.sugarcounter.features.entrySavingFeature.data.CheckThresholdResult
+import com.jumparoundcreations.sugarcounter.features.entrySavingFeature.data.CheckUserInputResult
 import com.jumparoundcreations.sugarcounter.features.entrySavingFeature.data.GetEntryByCategoryResult
 import com.jumparoundcreations.sugarcounter.features.entrySavingFeature.data.GramCountMode
 import com.jumparoundcreations.sugarcounter.features.entrySavingFeature.useCases.CheckDailyGramThresholdUseCase
@@ -35,21 +37,25 @@ class EntrySavingViewModelTest {
 
 
     private val category = mockk<Category>()
+    private lateinit var viewModel: EntrySavingViewModel
 
-    private val viewModel = EntrySavingViewModel(
-        mockScanBarcodeUseCase,
-        getEntryByCategoryUseCase,
-        saveEntryInDatabaseUseCase,
-        saveCategoryInDatabaseUseCase,
-        checkUserInputUseCase,
-        checkForDefaultSavingValuesUseCase,
-        displayAllCategoriesUseCase,
-        checkDailyGramThresholdUseCase
-    )
 
     @Before
     fun setup() {
+        viewModel = EntrySavingViewModel(
+            mockScanBarcodeUseCase,
+            getEntryByCategoryUseCase,
+            saveEntryInDatabaseUseCase,
+            saveCategoryInDatabaseUseCase,
+            checkUserInputUseCase,
+            checkForDefaultSavingValuesUseCase,
+            displayAllCategoriesUseCase,
+            checkDailyGramThresholdUseCase
+        )
+
         every { displayAllCategoriesUseCase() } returns flowOf(listOf(category))
+        every { saveEntryInDatabaseUseCase(any()) } returns Unit
+        every { saveCategoryInDatabaseUseCase(any()) } returns Unit
     }
 
 
@@ -226,6 +232,153 @@ class EntrySavingViewModelTest {
             expected = entryFieldQuantity,
             actual = viewModel.entrySavingStates.value.entryFieldQuantity
         )
+    }
+
+    @Test
+    fun `test EntrySavingIntents SaveSugarEntry with NoCategoryGiven and DismissNoCategoryDataEnteredAlert`() =
+        runTest {
+            //Arrange
+            every { checkForDefaultSavingValuesUseCase(any()) } returns false
+            every { checkUserInputUseCase(any()) } returns CheckUserInputResult.NoCategoryGiven
+
+            //Act
+            viewModel.onAction(
+                action = EntrySavingIntents.SaveSugarEntry
+            )
+
+            //Assert
+            assertEquals(
+                expected = true,
+                actual = viewModel.entrySavingStates.value.savingProcessMissingCategoryData
+            )
+
+            //Act
+            viewModel.onAction(
+                action = EntrySavingIntents.DismissNoCategoryDataEnteredAlert
+            )
+
+            //Arrange
+            assertEquals(
+                expected = false,
+                actual = viewModel.entrySavingStates.value.savingProcessMissingCategoryData
+            )
+        }
+
+    @Test
+    fun `test EntrySavingIntents SaveSugarEntry with NoGramDataGivenButCategoryGiven and DismissNoSugarDataEnteredAlert`() =
+        runTest {
+            //Arrange
+            every { checkForDefaultSavingValuesUseCase(any()) } returns true
+            every { checkUserInputUseCase(any()) } returns
+                    CheckUserInputResult.NoGramDataGivenButCategoryGiven
+
+            //Act
+            viewModel.onAction(
+                action = EntrySavingIntents.SaveSugarEntry
+            )
+
+            //Assert
+            assertEquals(
+                expected = "",
+                actual = viewModel.entrySavingStates.value.barcodeNumber
+            )
+            assertEquals(
+                expected = true,
+                actual = viewModel.entrySavingStates.value.savingProcessMissingSugarData
+            )
+
+            //Act
+            viewModel.onAction(
+                action = EntrySavingIntents.DismissNoSugarDataEnteredAlert
+            )
+
+            //Assert
+            assertEquals(
+                expected = false,
+                actual = viewModel.entrySavingStates.value.savingProcessMissingSugarData
+            )
+
+        }
+
+    @Test
+    fun `test EntrySavingIntents SaveSugarEntry with InputDataComplete`() = runTest {
+        //Arrange
+        every { checkForDefaultSavingValuesUseCase(any()) } returns false
+        every { checkUserInputUseCase(any()) } returns
+                CheckUserInputResult.InputDataComplete
+        every { checkDailyGramThresholdUseCase(any()) } returns
+                CheckThresholdResult.WithinDailyThresholdBoundaries
+
+        //Act
+        viewModel.onAction(
+            action = EntrySavingIntents.SaveSugarEntry
+        )
+
+        //Assert
+        assertEquals(
+            expected = "",
+            actual = viewModel.entrySavingStates.value.barcodeNumber
+        )
+        assertEquals(
+            expected = CheckThresholdResult.WithinDailyThresholdBoundaries,
+            actual = viewModel.entrySavingStates.value.savingProcessDailyGramThreshold
+        )
+    }
+
+    @Test
+    fun `test EntrySavingIntents SaveSugarEntry with InputDataComplete and UserThresholdReaction`() =
+        runTest {
+            //Arrange
+            every { checkForDefaultSavingValuesUseCase(any()) } returns false
+            every { checkUserInputUseCase(any()) } returns
+                    CheckUserInputResult.InputDataComplete
+            every { checkDailyGramThresholdUseCase(any()) } returns
+                    CheckThresholdResult.DailyThresholdBreached
+
+
+            //Act
+            viewModel.onAction(
+                action = EntrySavingIntents.SaveSugarEntry
+            )
+
+            //Assert
+            assertEquals(
+                expected = "",
+                actual = viewModel.entrySavingStates.value.barcodeNumber
+            )
+            assertEquals(
+                expected = CheckThresholdResult.DailyThresholdBreached,
+                actual = viewModel.entrySavingStates.value.savingProcessDailyGramThreshold
+            )
+
+        }
+
+    @Test
+    fun `test EntrySavingIntents ClearInputFields`() = runTest {
+        //Act
+        viewModel.onAction(
+            action = EntrySavingIntents.ClearInputFields
+
+        )
+        //Assert
+        assertEquals(
+            expected = "",
+            actual = viewModel.entrySavingStates.value.categoryInField
+        )
+        assertEquals(
+            expected = "",
+            actual = viewModel.entrySavingStates.value.entryFieldGram
+        )
+        assertEquals(
+            expected = "",
+            actual = viewModel.entrySavingStates.value.entryFieldQuantity
+        )
+        assertEquals(
+            expected = 0,
+            actual = viewModel.entrySavingStates.value.gramCountModeTabIndex
+        )
+
+
     }
 
 }
