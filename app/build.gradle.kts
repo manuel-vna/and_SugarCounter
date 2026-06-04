1ae99f81e9f56da3464829ade49681ddc2d4d677
manuelvonau <vonau.manuel@gmail.com>
2026-06-04 20:44:13 +0200

    deprecations fixed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,13 +2,11 @@ import java.util.regex.Pattern
 
 plugins {
     alias(libs.plugins.androidApplication)
-    alias(libs.plugins.jetbrainsKotlinAndroid)
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.ksp)
     alias(libs.plugins.detekt)
     alias(libs.plugins.aboutLibraries)
     alias(libs.plugins.ktlint)
-    id("kotlin-kapt")
     id("androidx.room")
     id("de.mannodermaus.android-junit5")
 }
