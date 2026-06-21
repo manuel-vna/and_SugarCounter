80f27f8058389ba03f9cf36733a2e25927c65ac6
manuelvonau <vonau.manuel@gmail.com>
2026-06-21 17:37:24 +0200

    #51, removing unnecessary dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -163,7 +163,6 @@ tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
 dependencies {
     implementation(libs.androidx.concurrent.futures)
     implementation(libs.androidx.concurrent.futures.ktx)
-
     implementation(libs.androidx.material3.window.size.class1.android)
     testImplementation(libs.junit.jupiter)
     androidTestImplementation(libs.androidx.espresso.core)
